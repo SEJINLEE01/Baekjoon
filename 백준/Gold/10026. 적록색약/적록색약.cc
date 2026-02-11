f34a7832e536468c29230a3eb7bbba9e2a590cb8
LEESEJIN <107745260+SEJINLEE01@users.noreply.github.com>
2026-02-11 13:04:43 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -2,70 +2,71 @@
 #include <queue>
 using namespace std;
 
-char map[101][101]; // 넉넉하게 101
+char map[101][101];
 bool visited[101][101];
 int dx[] = { 1,0,-1,0 };
 int dy[] = { 0,1,0,-1 };
 queue <pair<int, int>> Q;
 int N;
-
-void BFS() {
-    while (!Q.empty()) {
-        int y = Q.front().first;
-        int x = Q.front().second;
-        Q.pop();
-        for (int d = 0; d < 4; d++) {
-            int nx = dx[d] + x;
-            int ny = dy[d] + y;
-            if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                if (!visited[ny][nx] && map[ny][nx] == map[y][x]) {
-                    visited[ny][nx] = true;
-                    Q.push({ ny, nx });
-                }
-            }
-        }
-    }
-}
+int zone = 0;
+int color_zone = 0;
+void BFS();
 
 int main() {
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) cin >> map[i][j];
-    }
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> map[i][j];
+		}
+	}
+	fill(&visited[0][0], &visited[100][100], false);
+	
 
-    int zone = 0, color_zone = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+			if (!visited[i][j]) {
+				visited[i][j] = true;
+				Q.push({ i,j });
+				BFS();
+				zone++;
+			}
+	}
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (map[i][j] == 'G')
+				map[i][j] = 'R';
+		}
+	}
 
-    // 1회차 초기화 및 탐색
-    for(int i=0; i<101; i++) for(int j=0; j<101; j++) visited[i][j] = false;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (!visited[i][j]) {
-                visited[i][j] = true;
-                Q.push({ i,j });
-                BFS();
-                zone++;
-            }
-        }
-    }
+	fill(&visited[0][0], &visited[100][100], false);
 
-    // 적록색약용 맵 변환
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) if (map[i][j] == 'G') map[i][j] = 'R';
-    }
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+			if (!visited[i][j]) {
+				visited[i][j] = true;
+				Q.push({ i,j });
+				BFS();
+				color_zone++;
+			}
+	}
+	cout << zone << " "<<color_zone;
+}
 
-    // 2회차 초기화 및 탐색
-    for(int i=0; i<101; i++) for(int j=0; j<101; j++) visited[i][j] = false;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (!visited[i][j]) {
-                visited[i][j] = true;
-                Q.push({ i,j });
-                BFS();
-                color_zone++;
-            }
-        }
-    }
-
-    cout << zone << " " << color_zone;
-    return 0;
+void BFS() {
+	while (!Q.empty()) {
+		int y = Q.front().first;
+		int x = Q.front().second;
+		Q.pop();
+		for (int d = 0; d < 4; d++) {
+			int nx = dx[d] + x;
+			int ny = dy[d] + y;
+			if(nx >= 0 && nx < N && ny >= 0 && ny < N) {
+				if (!visited[ny][nx] && map[ny][nx] == map[y][x]) {
+					visited[ny][nx] = true; // 방문 처리
+					Q.push({ ny, nx });     // 다음 탐색을 위해 큐에 삽입
+				}
+			}
+		}
+	}
 }
