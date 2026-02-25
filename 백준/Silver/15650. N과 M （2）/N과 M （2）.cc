2cc91ccbc0c2b8127e3d34d3ed0fa7b824be28e9
LEESEJIN <107745260+SEJINLEE01@users.noreply.github.com>
2026-02-25 15:54:04 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> ans;
+
+void DFS(int start) {
+    // 숫자를 M개 다 골랐을 때 출력!
+    if (ans.size() == M) {
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << (i == ans.size() - 1 ? "" : " ");
+        }
+        cout << "\n";
+        return;
+    }
+
+    // start부터 N까지 루프
+    for (int i = start; i <= N; i++) {
+        ans.push_back(i);    // 숫자 넣기
+        DFS(i + 1);          // 다음 숫자는 무조건 지금보다 큰 값으로
+        ans.pop_back();      // 숫자 빼기 (백트래킹)
+    }
+}
+
+int main() {
+    // 입출력 속도 문제 방지
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    if (!(cin >> N >> M)) return 0;
+
+    DFS(1);
+
+    return 0;
+}
